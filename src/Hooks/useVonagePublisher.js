c3409894ece4b8c095d7900daf3bc87a0ba97e88
beinghaziq <beinghaziq@gmail.com>
2024-03-26 19:01:51 +0500

    resolved audio issue

--- src/Hooks/useVonagePublisher.js
+++ src/Hooks/useVonagePublisher.js
@@ -14,43 +14,55 @@ export const useVonagePublisher = (session) => {
   const targetLanguages = predefinedLanguages.map((language) => language.value);
 
   const createPublisher = () => {
-    const localPublishers ={};
-    for (let i = 0; i < targetLanguages.length; i++) {
-      const publisherOptions = {
-        insertMode: "append",
-        width: "100%",
-        height: "100%",
-        // Pass in the generated audio track as our custom audioSource
-        audioSource: null,
-        // Enable stereo audio
-        enableStereo: true,
-        // Increasing audio bitrate is recommended for stereo music
-        audioBitrate: 128000,
-        name: targetLanguages[i],
-      };
-  
-      let publisher = OT.initPublisher(
-        "publisher",
-        publisherOptions,
-        // eslint-disable-next-line no-loop-func
-        (error) => {
-          if (error) {
-            handleError(error);
-          } else {
-            // If the connection is successful, publish the publisher1 to the session
-            session.publish(publisher, (error) => {
-              console.log("session publish", publisher);
+    const audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)();
+    // Create audio stream from mp3 file and video stream from webcam
+    Promise.all([OT.getUserMedia({ videoSource: null })])
+      .then((results) => {
+        const [audioBuffer] = results;
+        const { audioStream } = createAudioStream(null, audioContext);
+        const localPublishers = {};
+        for (let i = 0; i < targetLanguages.length; i++) {
+          const publisherOptions = {
+            insertMode: "append",
+            width: "100%",
+            height: "100%",
+            // Pass in the generated audio track as our custom audioSource
+            audioSource: audioStream.getAudioTracks()[0],
+            // Enable stereo audio
+            enableStereo: true,
+            // Increasing audio bitrate is recommended for stereo music
+            audioBitrate: 128000,
+            name: targetLanguages[i],
+          };
+
+          let publisher = OT.initPublisher(
+            "publisher",
+            publisherOptions,
+            // eslint-disable-next-line no-loop-func
+            (error) => {
               if (error) {
                 handleError(error);
+              } else {
+                // If the connection is successful, publish the publisher1 to the session
+                session.publish(publisher, (error) => {
+                  console.log("session publish", publisher);
+                  if (error) {
+                    handleError(error);
+                  }
+                });
               }
-            });
-          }
+            }
+          );
+          localPublishers[targetLanguages[i]] = publisher;
         }
-      );
-      localPublishers[targetLanguages[i]] = publisher
-    }
-    setPublishers(localPublishers);
-  }
+        setPublishers(localPublishers);
+      })
+      .catch((error) => {
+        audioContext.close();
+        throw error;
+      });
+  };
 
   console.log("all publisher", publishers);
 
@@ -72,53 +84,59 @@ export const useVonagePublisher = (session) => {
         const { audioStream } = createAudioStream(audioBuffer, audioContext);
 
         targetLanguages.forEach((i) => {
-            if (!publishers[i]) {
-              const publisherOptions = {
-                insertMode: "append",
-                width: "100%",
-                height: "100%",
-                // Pass in the generated audio track as our custom audioSource
-                audioSource: audioStream ? audioStream.getAudioTracks()[0] : null,
-                // Enable stereo audio
-                enableStereo: true,
-                // Increasing audio bitrate is recommended for stereo music
-                audioBitrate: 128000,
-                name: targetLanguages[i],
-              };
-    
-              publishers[i] = OT.initPublisher(
-                "publisher",
-                publisherOptions,
-                // eslint-disable-next-line no-loop-func
-                (error) => {
-                  if (error) {
-                    handleError(error);
-                  } else {
-                    // If the connection is successful, publish the publisher1 to the session
-                    session.publish(publishers[i], (error) => {
-                      if (error) {
-                        handleError(error);
-                      }
-                    });
-                  }
+          if (!publishers[i]) {
+            const publisherOptions = {
+              insertMode: "append",
+              width: "100%",
+              height: "100%",
+              // Pass in the generated audio track as our custom audioSource
+              audioSource: audioStream ? audioStream.getAudioTracks()[0] : null,
+              // Enable stereo audio
+              enableStereo: true,
+              // Increasing audio bitrate is recommended for stereo music
+              audioBitrate: 128000,
+              name: targetLanguages[i],
+            };
+
+            publishers[i] = OT.initPublisher(
+              "publisher",
+              publisherOptions,
+              // eslint-disable-next-line no-loop-func
+              (error) => {
+                if (error) {
+                  handleError(error);
+                } else {
+                  // If the connection is successful, publish the publisher1 to the session
+                  session.publish(publishers[i], (error) => {
+                    if (error) {
+                      handleError(error);
+                    }
+                  });
                 }
-              );
-            } else {
-              console.log("Publishing the audio....", websocketTargetLanguage, targetLanguages[i]);
-              // If publisher1 is already initialized, update the audio source
-              if (websocketTargetLanguage === i) {
-                console.log("===============");
-                sendCaption(session, CaptionText, userTargetLanguage, websocketTargetLanguage);
-                publishers[i].publishAudio(false); // Stop publishing audio temporarily
-                publishers[i].setAudioSource(audioStream.getAudioTracks()[0]); // Set new audio source
-                publishers[i].publishAudio(true); // Start publishing audio again
-                publishers[i].publishCaptions(true);
               }
+            );
+          } else {
+            console.log(
+              "Publishing the audio....",
+              websocketTargetLanguage,
+              targetLanguages[i]
+            );
+            // If publisher1 is already initialized, update the audio source
+            if (websocketTargetLanguage === i) {
+              console.log("===============");
+              sendCaption(
+                session,
+                CaptionText,
+                userTargetLanguage,
+                websocketTargetLanguage
+              );
+              publishers[i].publishAudio(false); // Stop publishing audio temporarily
+              publishers[i].setAudioSource(audioStream.getAudioTracks()[0]); // Set new audio source
+              publishers[i].publishAudio(true); // Start publishing audio again
+              publishers[i].publishCaptions(true);
             }
           }
-      ) 
-        
-
+        });
 
         // console.log("Publishing the audio....",results, publishers[websocketTargetLanguage]);
         // // If publisher1 is already initialized, update the audio source
@@ -142,30 +160,30 @@ export const useVonagePublisher = (session) => {
   };
 
   // The following functions are used in functionality customization
-const toggleVideo = (state) => {
-  publishers.forEach(pub => {
-    pub.publishVideo(state);
-  });
-}
-const toggleAudio = (state) => {
-  publishers.forEach(pub => {
-    pub.publishAudio(state);
-  });
-}
-
-const togglePublisherDestroy = () => {
-  publishers.forEach(pub => {
-    pub.disconnect();
-  });
-}
-
-const stopStreaming = () => {
-  if (session) {
-    publishers.forEach(pub => {
-      session.unpublish(pub); 
+  const toggleVideo = (state) => {
+    publishers.forEach((pub) => {
+      pub.publishVideo(state);
     });
-  }
-}
+  };
+  const toggleAudio = (state) => {
+    publishers.forEach((pub) => {
+      pub.publishAudio(state);
+    });
+  };
+
+  const togglePublisherDestroy = () => {
+    publishers.forEach((pub) => {
+      pub.disconnect();
+    });
+  };
+
+  const stopStreaming = () => {
+    if (session) {
+      publishers.forEach((pub) => {
+        session.unpublish(pub);
+      });
+    }
+  };
 
   return {
     toggleVideo,
@@ -173,6 +191,6 @@ const stopStreaming = () => {
     togglePublisherDestroy,
     stopStreaming,
     createPublisher,
-    publishTranslatedAudio
+    publishTranslatedAudio,
   };
 };
